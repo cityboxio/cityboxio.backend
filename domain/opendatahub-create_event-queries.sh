2f3f8f6948619a407c51f012a4cbfb694259c7c3
zautomata <zautomata@gmail.com>
2018-11-15 02:44:45 +0200

    adds some notes

--- domain/opendatahub-create_event-queries.sh
+++ domain/opendatahub-create_event-queries.sh
@@ -6,6 +6,7 @@
 echo "EventCreated and stored to the eventstore"
 echo "This commands appends the eventstore with something $@"
 #echo "event_created bin json $1 `date`" >> /var/citybox/DATA/opendatahub.events
+citybox-redis koolevent "value of its `date`"
 logger -i -t citybox-events "event_created bin json $1 `date`" 
 
 #echo "event_created bin json $1 `date`" >> /var/log/cityboxio/citybox.events.log
